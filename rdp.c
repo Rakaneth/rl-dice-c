7ac5b0eba48a47d4876ce2a6124bceaffd54d0cb
Rakaneth <quincy.west.codes@outlook.com>
2024-04-19 16:30:00 -0400

    short-circuit next_int

--- rdp.c
+++ rdp.c
@@ -5,54 +5,63 @@
 #include <time.h>
 
 #define CUR(l) (l->str[l->cursor])
-#define IS_DIGIT(c) ((c) >= '0' && (c) <= '9' )
+#define IS_DIGIT(c) ((c) >= '0' && (c) <= '9')
 #define DIGIT(c) ((c) - '0')
 #define IS_EOF(c) ((c) == '\0')
 #define ADVANCE(l) (l->cursor++)
 
 static unsigned int rng_state = 0xDEADBEEF;
 
-void rng_init() {
+void rng_init()
+{
     rng_state = (unsigned int)time(NULL);
 }
 
-static unsigned int xorshift32() {
+static unsigned int xorshift32()
+{
     rng_state ^= rng_state << 13;
     rng_state ^= rng_state >> 17;
     rng_state ^= rng_state << 5;
     return rng_state;
 }
 
-typedef struct lex_t {
+typedef struct lex_t
+{
     size_t cursor;
-    char* str;
+    char *str;
 } Lex;
 
-static Lex* lex_create(const char* str) {
-    Lex* l = (Lex*)malloc(sizeof(Lex));
+static Lex *lex_create(const char *str)
+{
+    Lex *l = (Lex *)malloc(sizeof(Lex));
     l->cursor = 0;
     l->str = strdup(str);
     return l;
 }
 
-static void lex_destroy(Lex* l) {
+static void lex_destroy(Lex *l)
+{
     free(l->str);
     free(l);
 }
 
-static void lex_skip_ws(Lex* l) {
+static void lex_skip_ws(Lex *l)
+{
     char c = CUR(l);
-    while (c == ' ' || c == '\t') {
+    while (c == ' ' || c == '\t')
+    {
         ADVANCE(l);
         c = CUR(l);
     }
 }
 
-static int lex_num(Lex* l) {
+static int lex_num(Lex *l)
+{
     char c = CUR(l);
     int acc = 0;
 
-    while (IS_DIGIT(c)) {
+    while (IS_DIGIT(c))
+    {
         acc = acc * 10 + DIGIT(c);
         ADVANCE(l);
         c = CUR(l);
@@ -61,38 +70,46 @@ static int lex_num(Lex* l) {
     return acc;
 }
 
-static int expr(Lex* l);
-static int term(Lex* l);
-static int dice(Lex* l, int num);
+static int expr(Lex *l);
+static int term(Lex *l);
+static int dice(Lex *l, int num);
 
-int next_int(int low, int high) {
+int next_int(int low, int high)
+{
+    if (low == high)
+        return low;
     int range = high - low + 1;
     int r = xorshift32() % range;
     return r + low;
 }
 
-int die_roll(int num, int sides) {
+int die_roll(int num, int sides)
+{
     int n = (num < 1) ? 1 : num;
     int s = (sides < 1) ? 1 : sides;
     int acc = 0;
 
-    for (int i=0; i<n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         acc += next_int(1, s);
     }
 
     return acc;
 }
 
-bool next_bool() {
+bool next_bool()
+{
     return (xorshift32() & 1) == 1;
 }
 
-static void print_err(Lex* l, const char* msg) {
+static void print_err(Lex *l, const char *msg)
+{
     fprintf(stderr, "Parsing error at char %zu (%c): %s", l->cursor, CUR(l), msg);
     exit(1);
 }
 
-static int expr(Lex* l) {
+static int expr(Lex *l)
+{
     int a = 0;
     char c = CUR(l);
 
@@ -100,13 +117,17 @@ static int expr(Lex* l) {
 
     a = term(l);
     c = CUR(l);
-    while (!(IS_EOF(c))) {
+    while (!(IS_EOF(c)))
+    {
         lex_skip_ws(l);
         c = CUR(l);
-        if (c == '+') {
+        if (c == '+')
+        {
             ADVANCE(l);
-            a = a + term(l);            
-        } else if (c == '-') {
+            a = a + term(l);
+        }
+        else if (c == '-')
+        {
             ADVANCE(l);
             a = a - term(l);
         }
@@ -115,23 +136,30 @@ static int expr(Lex* l) {
     return a;
 }
 
-static int term(Lex* l) {
+static int term(Lex *l)
+{
     int a = 0;
     char c = 0;
 
     lex_skip_ws(l);
     c = CUR(l);
 
-    if (c == 'd') {
+    if (c == 'd')
+    {
         ADVANCE(l);
         return dice(l, 1);
-    } else if (IS_DIGIT(c)) {
+    }
+    else if (IS_DIGIT(c))
+    {
         a = lex_num(l);
         c = CUR(l);
-        if (c == 'd') {
+        if (c == 'd')
+        {
             ADVANCE(l);
             return dice(l, a);
-        } else {
+        }
+        else
+        {
             return a;
         }
     }
@@ -140,11 +168,13 @@ static int term(Lex* l) {
     return -1;
 }
 
-static int dice(Lex* l, int num) {
+static int dice(Lex *l, int num)
+{
     char c = CUR(l);
     int b = 0;
 
-    if (!IS_DIGIT(c)) {
+    if (!IS_DIGIT(c))
+    {
         print_err(l, "expected digit");
     }
 
@@ -152,23 +182,21 @@ static int dice(Lex* l, int num) {
     return die_roll(num, b);
 }
 
-int roll(const char* str) {
-    Lex* lex = lex_create(str);
+int roll(const char *str)
+{
+    Lex *lex = lex_create(str);
     int result = expr(lex);
     lex_destroy(lex);
     return result;
 }
 
-int gauss_int(int median) {
+int gauss_int(int median)
+{
     int acc = 0;
     int sides = median * 2;
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         acc += die_roll(1, sides);
     }
     return acc / 3;
 }
-
-
-
-
-
